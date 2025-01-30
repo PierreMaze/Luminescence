3011f1bd4e16a65c9d431de3e2955c755536ee18
Pierre M. <promazelaygue@gmail.com>
2025-01-31 00:40:19 +0100

    Hotfix: Moved Lumi.svg to src directory and updated import path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,11 @@
-import lumiLogo from '/lumi.svg';
-import './App.css';
+import lumiLogo from './assets/img/Lumi.svg';
 
 function App() {
   return (
     <>
       <div>
         <a href="https://luminescence33.com/" target="_blank">
-          <img src={lumiLogo} className="logo" alt="Vite logo" />
+          <img src={lumiLogo} className="logo" alt="Luminescence33 logo" />
         </a>
       </div>
       <h1>Luminescence</h1>
